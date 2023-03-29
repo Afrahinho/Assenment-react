616825890d47572c317b70f5a35af6ed725bfd01
unknown <elmiosman343@gmail.com>
2023-03-29 08:51:38 -0900

    first project

---
+++ src/Component/Center.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Center() {
+  return (
+    <div className='Center'>
+      <h1>MY NAME: Elmi osman AFrah🥰🥰</h1>
+      <h1>mY ID C120289🙂</h1>
+    </div>
+  );
+}
+
+export default Center;
